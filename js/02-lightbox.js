188365d05d445075e513b10659aa12af03c8ff07
Iryna-Somyuk <Iryna.Somuyk@gmail.com>
2022-11-05 00:36:15 +0200

    task_2_correction

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,14 +7,15 @@ const galleryImg = document.querySelector(".gallery");
 
 const markUpGallery = galleryItems.map(
   ({ preview, original, description }) =>
-    `<a class="gallery__item" href = "${original}">
+    `<li class="gallery__list"><a class="gallery__item" href = "${original}">
 <img class= "gallery__image" src = "${preview}" 
- alt = "${description}"/></a>`
+ alt = "${description}"/></a></li>`
 );
 galleryImg.insertAdjacentHTML("beforeend", markUpGallery.join(""));
 
 //console.log(SimpleLightbox);
 new SimpleLightbox(".gallery a", {
+  captionPosition: 'bottom',
   captionsData: "alt",
   captionDelay: 250,
 });
